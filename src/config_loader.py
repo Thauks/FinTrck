f19676f58919179f3d964c0c70899809c7c3d88a
Jordi Mestres <jordimestrees@gmail.com>
2024-10-19 04:22:37 +0200

    Improving security for pwds and adding the config files for each platform. Also improved the design of fetching data

--- src/config_loader.py
+++ src/config_loader.py
@@ -1,9 +1,13 @@
-import yaml
+import yaml, os, random
 from src.models.myinvestor import MyinvestorConfig, MyLoginInfo, MyEndpoints
 
 def load_myinvestor_config(config_file) -> MyinvestorConfig:
     with open(config_file, 'r') as f:
         data = yaml.safe_load(f)
+        
+        data['login_info']['customerId'] = os.environ.get('DNI')
+        data['login_info']['password'] = os.environ.get('MYINVESTOR_PWD')
+        data['login_info']['deviceId'] = os.environ.get('MYINVESTOR_DEVICE')
         
         # Unpack the data into the dataclass structure
         login_info = MyLoginInfo(**data['login_info'])
@@ -11,5 +15,6 @@ def load_myinvestor_config(config_file) -> MyinvestorConfig:
 
         return MyinvestorConfig(
             login_info=login_info,
-            endpoints=endpoints
+            endpoints=endpoints,
+            data_mapping=data['data_mapping']
         )
